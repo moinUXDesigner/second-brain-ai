1012134e8049a60faafd08c4daece62b18cb6b8b
SHAIK KHAJA MYNUDDIN <110783353+moinUXDesigner@users.noreply.github.com>
2026-04-28 14:27:08 +0530

    Replace 'My State' with 'Projects' in bottom navigation bar

--- src/components/layout/BottomNav.tsx
+++ src/components/layout/BottomNav.tsx
@@ -21,9 +21,9 @@ const rightItems = [
     icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4',
   },
   {
-    to: '/daily-state',
-    label: 'My State',
-    icon: 'M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+    to: '/projects',
+    label: 'Projects',
+    icon: 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
   },
 ];
 
